72ff43ed510b3523b869e57c0ecf67473dfecd13
anna markovich <aamogilnaya_1@edu.hse.ru>
2024-10-13 00:24:36 +0300

    fixed pitch aug

--- src/transforms/wav_augs/pitch.py
+++ src/transforms/wav_augs/pitch.py
@@ -3,9 +3,11 @@ from torch import Tensor, nn
 
 
 class PitchShift(nn.Module):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, sample_rate, n_steps, *args, **kwargs):
         super().__init__()
-        self._aug = torch_audiomentations.PitchShift(*args, **kwargs)
+        self._aug = torch_audiomentations.PitchShift(
+            sample_rate, n_steps, *args, **kwargs
+        )
 
     def __call__(self, data: Tensor):
         x = data.unsqueeze(1)
