a440c6771a5adaf99a6d44c12e368344a35c02b8
anna markovich <aamogilnaya_1@edu.hse.ru>
2024-10-12 22:39:29 +0300

    fixed data_dir

--- src/datasets/librispeech_dataset.py
+++ src/datasets/librispeech_dataset.py
@@ -66,6 +66,7 @@ class LibrispeechDataset(BaseDataset):
             with index_path.open() as f:
                 index = json.load(f)
         else:
+            index_path = self._data_dir_write / f"{part}_index.json"
             index = self._create_index(part)
             with index_path.open("w") as f:
                 json.dump(index, f, indent=2)
