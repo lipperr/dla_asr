2f2d80aab310194b70b6eac7973f4a91b66f5356
anna markovich <aamogilnaya_1@edu.hse.ru>
2024-10-13 07:09:50 +0300

    params for fading

--- src/transforms/wav_augs/fading.py
+++ src/transforms/wav_augs/fading.py
@@ -3,12 +3,12 @@ from torch import Tensor, nn
 
 
 class Fading(nn.Module):
-    def __init__(self, fade_in_len, fade_out_len, *args, **kwargs):
+    def __init__(self, alpha=0.5, *args, **kwargs):
         super().__init__()
-        self._aug = torchaudio.transforms.Fade(
-            fade_in_len, fade_out_len, *args, **kwargs
-        )
+        self.alpha = alpha
 
     def __call__(self, data: Tensor):
+        fade_len = data.shape[-1] * self.alpha // 1
+        self._aug = torchaudio.transforms.Fade(fade_len, fade_len)
         x = data.unsqueeze(1)
         return self._aug(x).squeeze(1)
